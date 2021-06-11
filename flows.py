bug fix with gradient

flows.py
@@ -89,7 +89,7 @@ class AffineCouplingFlow(Flow):
         z   = args[0]
         z_d = z[:,:self.d]
         '''summation under equation 6'''
-        return torch.sum(torch.exp(self.shift_log_scale(z_d))[:, 1:],dim=-1)
+        return torch.sum(self.shift_log_scale(z_d)[:, 1:],dim=-1)
 
 
 class ReverseFlow(Flow):
@@ -137,7 +137,7 @@ class Norm_flow_model(nn.Module):
           self.bijectors = nn.ModuleList(bijectros)
 
           '''Normilizing flow probability transformation
-          reprsents [f_1,f_2,...,f_n]'''
+          reprsents [f_1,f_2,...,f_n](inv)'''
           self.transforms = transform.ComposeTransform(bijectros)
 
           '''density of variable z_0, with known density function, usually mult var gauss with diag covar matrix'''
@@ -146,8 +146,6 @@ class Norm_flow_model(nn.Module):
           '''density of target variable z_n = f_n * f_(n_1) **** f_1(z_0)'''
           self.final_density = distrib.TransformedDistribution(density, self.transforms)
 
-          '''list : [log_det_df_i/dz_(i-1) for i in range n] , where n is the flow number'''
-          self.log_det = []
 
       def forward(self,x):
           '''Input: Real data sample X
@@ -158,7 +156,8 @@ class Norm_flow_model(nn.Module):
 
           z = x
           l = len(self.bijectors)
+          list_log_det = []
           for i in range(l):
               z = self.bijectors[l-1-i]._inverse(z)
-              self.log_det.append(self.bijectors[l-1-i].log_abs_det_jacobian(z))
-          return z, self.log_det
+              list_log_det.append(self.bijectors[l-1-i].log_abs_det_jacobian(z))
+          return z, list_log_det

test.py
@@ -19,22 +19,22 @@ x = torch.tensor(sample_gaus_mixture(w_list,means,covar_list,n_samples),device=d
 ref_distr    = distrib.MultivariateNormal(torch.zeros(2), torch.eye(2))
 z            = ref_distr.sample(sample_shape=(32,))
 
-model_affine = AffineCouplingFlow(2,n_hidden=256,n_layers=3)
+model_affine = AffineCouplingFlow(2,n_hidden=256,n_layers=3,device=device)
 
 
 print(model_affine.shift_log_scale)
 
 Blocks       = [AffineCouplingFlow,ReverseFlow]*5+[AffineCouplingFlow]
 print(Blocks)
-model_mult   = Norm_flow_model(2,Blocks,ref_distr)
+model_mult   = Norm_flow_model(2,Blocks,ref_distr,device)
 
-
+'''
 print('Single layer test')
 print('Inverse affine call,f_inv(x)={}'.format(model_affine._inverse(x)))
 print('Forward affine call,f(z)={}'.format(model_affine(z)))
 print('log_det_df_z_0={}'.format(model_affine.log_abs_det_jacobian(z)))
 
-'''
+
 
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
@@ -52,7 +52,7 @@ plt.show()
 print('Multi flow test')
 z_0, list_logdet_jacob = model_mult(x)
 print('mult flow z_0={}'.format(z_0))
-print('mult flow, jacob list,f(z)={}'.format(list_logdet_jacob))
+print('mult flow, jacob list ={}'.format(list_logdet_jacob))
 
 
 '''

train.py
@@ -11,22 +11,20 @@ epochs  = 10000
 plot_it = 1000
 n_samples  = 128
 learning_rate = 1e-4
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 ref_distr    = distrib.MultivariateNormal(torch.zeros(2), torch.eye(2))
 Blocks       = [AffineCouplingFlow,ReverseFlow]*5+[AffineCouplingFlow]
-model        = Norm_flow_model(2,Blocks,ref_distr)
+model        = Norm_flow_model(2,Blocks,ref_distr,device)
 optimizer    = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 for i in range(epochs):
-
+    optimizer.zero_grad()
     noisy_moons = datasets.make_moons(n_samples=128, noise=.05)[0].astype(np.float32)
     X = StandardScaler().fit_transform(noisy_moons)
     X = torch.FloatTensor(X,device=device)
     z_0,list_log_det = model(X)
     l                = loss(ref_distr,z_0,list_log_det)
-    optimizer.zero_grad()
     l.backward()
     optimizer.step()
 